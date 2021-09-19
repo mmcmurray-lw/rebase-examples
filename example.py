7bf0922ceae4cd1e503854b26110ba0234c43738
Mark McMurray <mark.mcmurray@lacework.net>
2021-09-19 21:00:09 +0100

    feat: add part 4 and 5
    Signed-off-by: Mark McMurray <mark.mcmurray@lacework.net>

--- example.py
+++ example.py
@@ -10,13 +10,16 @@ def part3():
 def part4():
     print("This is part4!")
 
+def part5():
+    print("This is part5!")
+
 def main():
     print("Hello World!")
     part1()
     part2()
     part3()
-    #TODO - add call for part 4
-    #TODO - add part 5 
+    part4()
+    part5()
 
 if __name__ == "__main__":
     main()
