81f053f82c2146c2897d74e213b1b1474bf4b43e
Mark McMurray <mark.mcmurray@lacework.net>
2021-09-19 20:58:55 +0100

    feat: WIP add part 4 and 5
    Signed-off-by: Mark McMurray <mark.mcmurray@lacework.net>

--- example.py
+++ example.py
@@ -7,11 +7,16 @@ def part2():
 def part3():
     print("This is part3!")
 
+def part4():
+    print("This is part4!")
+
 def main():
     print("Hello World!")
     part1()
     part2()
     part3()
+    #TODO - add call for part 4
+    #TODO - add part 5 
 
 if __name__ == "__main__":
     main()
