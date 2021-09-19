a1d217522d5627bdc9bf0a72f0453b07338f0395
Mark McMurray <mark.mcmurray@lacework.net>
2021-09-19 20:56:03 +0100

    feat: add part3 and change messages
    Signed-off-by: Mark McMurray <mark.mcmurray@lacework.net>

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
 def part1():
-    print("part1!")
+    print("This is part1!")
 
 def part2():
-    print("part2!")
+    print("This is part2!")
 
+def part3():
+    print("This is part3!")
 
 def main():
     print("Hello World!")
     part1()
     part2()
+    part3()
 
 if __name__ == "__main__":
     main()
