f9185a80882855723ddbb06de604679d7f1a243f
Mark McMurray <mark.mcmurray@lacework.net>
2021-09-21 09:58:38 +0100

    feat: add example 2
    Signed-off-by: Mark McMurray <mark.mcmurray@lacework.net>

---
+++ example2.py
@@ -0,0 +1,15 @@
+def part1():
+    print("part1!")
+
+def part2():
+    print("part2!")
+
+
+def main():
+    print("Hello World!")
+    part1()
+    part2()
+
+if __name__ == "__main__":
+    main()
+
